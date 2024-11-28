c9d18d200307c2f94db2802cf7b3918a391bce57
medmede7 <mederic.boistard@gmail.com>
2024-11-28 19:29:42 +0100

    Changes to be committed: 	new file:   .Module_Template.py.swp 	modified:   Module_Template.py 		full script not debugged 	deleted:    Module_Template.py!

--- Module_Template.py
+++ Module_Template.py
@@ -63,26 +63,27 @@ Global_My_Global_Var = 0
 class my_class:
 
 #docString:
-"""NAME
-	my_class
-SYNOPSYS
-	my_object = myclass()
-DESCRIPTION
-	Describe here help for the class
-AUTHOR
-	name/alias
-REPORTING BUGS
-	Date - bug description
-EXAMPLE
-	my_object = myclass
-SEE ALSO
-
-"""
+	"""
+	NAME
+		my_class
+	SYNOPSYS
+		my_object = myclass()
+	DESCRIPTION
+		Describe here help for the class
+	AUTHOR
+		name/alias
+	REPORTING BUGS
+		Date - bug description
+	EXAMPLE
+		my_object = myclass
+	SEE ALSO
+	
+	"""
 #	Attribute
 # - define attribute here -
 #	
 # 	Constructor
-	def __init__()
+	def __init__():
 		pass
 #	Method
 # ' define other method here
@@ -103,25 +104,34 @@ SEE ALSO
 def TestFunction_my_class():
 
 #docString:
-"""NAME
-	TestFunction_my_class
-SYNOPSYS
-	def TestFunction_my_class():
-DESCRIPTION
-	Describe here help for the function
-AUTHOR
-	name/alias
-REPORTING BUGS
-	Date - bug description
-EXAMPLE
-	my_object = myclass                
-SEE ALSO                                   
-"""                                           
+	"""
+	NAME
+		TestFunction_my_class
+	SYNOPSYS
+		def TestFunction_my_class():
+	DESCRIPTION
+		Describe here help for the function
+	AUTHOR
+		name/alias
+	REPORTING BUGS
+		Date - bug description
+	EXAMPLE
+		returnValue = TestFunction_my_class()                
+	SEE ALSO                                   
+	"""    	                                       
+#variable
+#	declare your variable here	
+	myVar = 0
+#process
 
 	my_object = my_class()
+	if isinstance(my_object,my_class):
+		return 'my_object is well created :)'
+#return	
+	return 'my_object is not created :('
 
 #
-#			-- CLASS TEST FUNCTION my_class			#
+#		-- END OF CLASS TEST FUNCTION my_class	--		#
 #	========================================================	#	
 #########################################################################
 #															#
@@ -137,28 +147,104 @@ SEE ALSO
 def MyFunction():
 
 #docString:
-"""NAME
-	MyFunction	
-SYNOPSYS
-	MyFunction(param1, param2)
-DESCRIPTION
-	Describe here help for the function
-AUTHOR
-	name/alias
-REPORTING BUGS
-	Date - bug description
-EXAMPLE
-	my_object = myclass                
-SEE ALSO                                   
-"""                                           
+	"""
+	NAME
+		MyFunction	
+	SYNOPSYS
+		MyFunction(param1, param2)
+	DESCRIPTION
+		Describe here help for the function
+	AUTHOR
+		name/alias
+	REPORTING BUGS
+		Date - bug description
+	EXAMPLE
+		returnValue = MyFunction(param1, param2)                
+	SEE ALSO                                   
+	"""                                           
 
+#variable
+#	declare your variable here
+	myVar = 0
+#process
+	pass
+# return
+	return 0
+#
+#			--  END OF FUNCTION MyFunction --		#
+#	========================================================	#	
+#########################################################################
+#															#
+#					-- END OF FUNCTION SECTION --							#
+#########################################################################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#########################################################################################################################
+#					-- TEST FUNCTION SECTION --							#
+#															#
+#	========================================================	#	
+#		-- TEST FUNCTION TestMyFunction --			#
+#	Test Function  docString: define usages of the test function
+def TestMyFunction():
 
-
-
-
+#docString:
+	"""
+	NAME
+		TestMyFunction	
+	SYNOPSYS
+		TestMyFunction()
+	DESCRIPTION
+		Describe here help for the test function
+	AUTHOR
+		name/alias
+	REPORTING BUGS
+		Date - bug description
+	EXAMPLE
+		TestMyFunction()                
+	SEE ALSO                                   
+	"""                                       
+                                                                                                                        
+# Variable                                                                                                               	
+#	declare your variable here                                                                                      	
+	myVar = 0                                                                                                        	
+#process
+	if MyFunction(1,2):
+		return 'test of Myfunction is pass :)'                                                                                                        
+# return
+	return 'test of MyFunction is fail :(' 
+#
+#		--  END OF TEST FUNCTION MyFunction --		#
+#	========================================================	#	
+#########################################################################
+#															#
+#					-- END OF TEST FUNCTION SECTION --						#
+#########################################################################################################################
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#########################################################################################################################
+#					-- MAIN SECTION --								#
+#															#
 
 if __name__=="__main__":
-	module = Path(__file__)
 	print('module ', module.name, ' start process ', __name__)
-	pass
-	print('module ', module.name, ' end process ', __name__)
+	module = Path(__file__)
+#main process
+	print(TestFunction_my_class())
+	print(TestFunction_my_class())	
+
+#end of main process
+	
+ 	print('module ', module.name, ' end process ', __name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
